97271076dc2626bbff6674bd451e5a950a9c1dcb
Qwib-Qwib <jp93rom@orange.fr>
2022-06-20 20:25:00 +0200

    Fix a typo in the old cypher_key variable name.

--- caesar-cypher.rb
+++ caesar-cypher.rb
@@ -1,24 +1,24 @@
 require 'pry-byebug'
 
-def caesar_cipher (string, cypher_key)
+def caesar_cipher (string, cipher_key)
   original_text_array = string.split("")
   new_text_array = original_text_array.map do |char|
     if char.upcase == char
       char = char.downcase
-      char = character_conversion(char, cypher_key)
+      char = character_conversion(char, cipher_key)
       char = char.upcase
     else
-      character_conversion(char, cypher_key)
+      character_conversion(char, cipher_key)
     end
   end
   p new_text_array.join("")
 end
 
-def character_conversion (char, cypher_key)
+def character_conversion (char, cipher_key)
   if (97..122).include?(char.codepoints[0]) == false then char = char
-  elsif char.codepoints[0] + cypher_key > 122 then char = ((char.codepoints[0] + cypher_key) - 26).chr
-  elsif char.codepoints[0] + cypher_key < 97 then char = ((char.codepoints[0] + cypher_key) + 26).chr
-  else char = (char.codepoints[0] + cypher_key).chr
+  elsif char.codepoints[0] + cipher_key > 122 then char = ((char.codepoints[0] + cipher_key) - 26).chr
+  elsif char.codepoints[0] + cipher_key < 97 then char = ((char.codepoints[0] + cipher_key) + 26).chr
+  else char = (char.codepoints[0] + cipher_key).chr
   end
   return char
 end
